2ed61fc1067eeda8458f991e57fa6ab441b865e1
Venugh <venugh@gmx.net>
2012-07-08 17:41:13 +0200

    Core: Fix config setting, need test.

--- src/server/collision/Management/MMapFactory.cpp
+++ src/server/collision/Management/MMapFactory.cpp
@@ -39,9 +39,7 @@ namespace MMAP
     bool MMapFactory::IsPathfindingEnabled(uint32 mapId)
     {
         if (DisableMgr::IsDisabledFor(DISABLE_TYPE_MAP, mapId, NULL))
-            return false;
-
-        return sWorld->getBoolConfig(CONFIG_ENABLE_MMAPS);
+            return sWorld->getBoolConfig(CONFIG_ENABLE_MMAPS);
     }
 
     void MMapFactory::clear()
