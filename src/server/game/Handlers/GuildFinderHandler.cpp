ef8b84aea96a3af8f43d85a0d5f9a1631f70a621
Venugh <venugh@gmx.net>
2012-08-16 15:41:44 +0200

    Core/Guild: Start work on GuildFinder.

---
+++ src/server/game/Handlers/GuildFinderHandler.cpp
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2008-2012 TrinityCore <http://www.trinitycore.org/>
+ * Copyright (C) 2005-2009 MaNGOS <http://getmangos.com/>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "WorldPacket.h"
+#include "Object.h"
+
+void HandleGuildFinderBrowse(WorldPacket & recvData)
+{
+}
+
+void HandleGuildFinderSetGuildPost(WorldPacket & recvData) {}
+
+void HandleGuildFinderPostUpdated(WorldPacket & recvData) {}
+
+void HandleGuildFinderCommandResult(WorldPacket & recvData) {}
+
+void HandlerLFGuildBrowseUpdated(WorldPacket & recvData) {}
+
+void HandlerLFGuildGetRecruits(WorldPacket & recvData) {}
+
+void HandleLFGuildRecruitListUpdated(WorldPacket & recvData) {}
+
+void HandleLFGuildMembershipListUpdated(WorldPacket & recvData) {}
+
+void HandleLFGuildDeclineRecruit(WorldPacket & recvData)
+{
+    sLog->outDebug(LOG_FILTER_NETWORKIO, "WORLD: Received CMSG_LF_GUILD_DECLINE_RECRUIT");
+
+    ObjectGuid playerGuid;
+
+    playerGuid[1] = recvData.ReadBit(); 
+    playerGuid[4] = recvData.ReadBit();
+    playerGuid[5] = recvData.ReadBit();
+    playerGuid[2] = recvData.ReadBit();
+    playerGuid[6] = recvData.ReadBit();
+    playerGuid[7] = recvData.ReadBit();
+    playerGuid[0] = recvData.ReadBit();
+    playerGuid[3] = recvData.ReadBit();
+
+    recvData.ReadByteSeq(playerGuid[5]);
+    recvData.ReadByteSeq(playerGuid[7]);
+    recvData.ReadByteSeq(playerGuid[2]);
+    recvData.ReadByteSeq(playerGuid[3]);
+    recvData.ReadByteSeq(playerGuid[4]);
+    recvData.ReadByteSeq(playerGuid[1]);
+    recvData.ReadByteSeq(playerGuid[0]);
+    recvData.ReadByteSeq(playerGuid[6]);
+}
+
+void HandleLFGuildRemoveRecruit(WorldPacket & recvData) {}
